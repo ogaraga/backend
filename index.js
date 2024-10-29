844b5c17137a024d56fdb1c215d4a06bbfcaa218
Ogaraga <raymond.ogaraga@gmail.com>
2024-10-29 11:51:53 +0100

    index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://frontends-psi.vercel.app",
+    origin: ["https://frontends-psi.vercel.app"],
     methods: ['POST', 'GET', 'PUT', 'DELETE'],
     credentials: true
 }));
